db444a66a237d977d36f2e1ac10aad04d4af7e25
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-09 02:43:03 +0300

    .

--- putin_web/urls.py
+++ putin_web/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, url
+from django.urls import path, include, re_path
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    url(r'^discord/', include('discord_bind.urls')),
+    re_path(r'^discord/', include('discord_bind.urls')),
 	path('register/', user_views.register, name='register'),
 ]
