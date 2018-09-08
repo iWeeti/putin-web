14a6808a8e7e9108e143ba957e64c2f686ff61f1
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-09 00:53:00 +0300

    .

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'CoreyMS',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'September 8, 2018'
-	},
-	{
-		'author': 'Veeti',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'September 9, 2018'
-	}
-]
+from .models import Post
 
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context=context)
 
