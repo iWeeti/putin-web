246e2e01816bd1c6b04d7d0a294b714b114ec10c
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-09 04:11:58 +0300

    .

--- putin_web/settings.py
+++ putin_web/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'putin_web.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    '_default': {
         'NAME': 'test.db',
         'ENGINE': 'django.db.backends.sqlite3',
     },
-    'postgres': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'putin',
         'USER': 'putin',
