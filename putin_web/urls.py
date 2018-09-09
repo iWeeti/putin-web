ef85863b3e408d99979d36d52320acf5117654b3
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-09 10:40:54 +0300

    .

--- putin_web/urls.py
+++ putin_web/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     re_path(r'^discord/', include('discord_bind.urls')),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 	path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
