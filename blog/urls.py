2940fc92dda7435115b98b9aaf7bfb978546bbcc
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-09 13:32:11 +0300

    .

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from .views import PostListView
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='blog-home'),
-	path('posts/', PostListView.as_view(), name='blog-posts'),
+	# path('', views.home, name='blog-home'),
+	path('', PostListView.as_view(), name='blog-posts'),
 	path('about/', views.about, name='blog-about'),
 	path('announcements/', views.announcements, name='blog-announcements'),
 ]
