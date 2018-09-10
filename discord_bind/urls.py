991a34b95946bbe99c9319cb88d2a8ca5798ba87
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-10 21:01:00 +0300

    .

--- discord_bind/urls.py
+++ discord_bind/urls.py
@@ -30,6 +30,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='login'),
+    url(r'^$', views.index, name='discord_bind_index'),
     url(r'^cb$', views.callback, name='discord_bind_callback'),
 ]
