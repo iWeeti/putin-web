39f34718d3491cdac797ce6e98bbc3d1942a65fb
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-11 13:51:56 +0300

    .

--- putin/views.py
+++ putin/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from blog.models import Announcement
 
 def index(request):
-	return render(request, 'putin/home.html')
+	ann = Announcement.objects.all()[::-1]
+	context = {
+		'ann': ann[0:2]
+	}
+	return render(request, 'putin/home.html', context)
