008f67c298a54f53ae701d72a5e0f1184d78c088
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-11 13:53:04 +0300

    .

--- putin/views.py
+++ putin/views.py
@@ -4,6 +4,6 @@ from blog.models import Announcement
 def index(request):
 	ann = Announcement.objects.all()[::-1]
 	context = {
-		'ann': ann[0:2]
+		'ann': ann[0:5]
 	}
 	return render(request, 'putin/home.html', context)
