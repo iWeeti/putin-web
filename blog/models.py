640a0c4fe4d4e81908f13b9bfc575310bd456fa0
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-13 18:13:59 +0300

    .

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
 	def db_for_write(self, model, **hints):
 		return 'default'
 
-    def db_for_read(self, model, **hints):
+	def db_for_read(self, model, **hints):
 		return 'default'
 	
 
