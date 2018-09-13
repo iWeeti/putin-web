7ebf0bdfabd3f4440b27d72ab03eea2914145af5
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-13 16:57:41 +0300

    .

--- putin/views.py
+++ putin/views.py
@@ -7,7 +7,7 @@ def index(request):
 	ann = Announcement.objects.all()[::-1]
 	settings = Settings.objects.using('bot').all()[0]
 	context = {
-		'ann': ann[0:5],
-		'settings': '123'
+		'ann': '1',
+		'settings': '123',
 	}
 	return render(request, 'putin/home.html', context)
