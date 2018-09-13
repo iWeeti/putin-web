093fe4407d7d6ed20066e2c0b62b2f77e80efdb1
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-13 09:20:55 +0300

    .

--- putin/views.py
+++ putin/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from blog.models import Announcement
+from blog.models import Announcement, Feeds
 import psycopg2
 
 def index(request):
 	ann = Announcement.objects.all()[::-1]
+	feeds = Feeds.objects.all()
 	context = {
-		'ann': ann[0:5]
+		'ann': ann[0:5],
+		'feeds': feeds
 	}
 	return render(request, 'putin/home.html', context)
