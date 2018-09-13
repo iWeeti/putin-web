fcb4d182622669bd7ab251e03111e4f269561d01
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-13 20:02:14 +0300

    :

--- putin/views.py
+++ putin/views.py
@@ -14,7 +14,7 @@ def index(request):
 @login_required
 def profile(request):
 	ann = Announcement.objects.all()[::-1]
-	if request.GET:
+	if not request.GET is None:
 		try:
 			profile = Profiles.objects.using('bot').get(id=request.GET['uid'])
 		except:
