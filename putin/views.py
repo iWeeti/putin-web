fce5bba8204d42ebc2a5277cf1c0ecaf1202d19d
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-13 17:42:52 +0300

    .

--- putin/views.py
+++ putin/views.py
@@ -6,7 +6,7 @@ import psycopg2
 def index(request):
 	ann = Announcement.objects.all()[::-1]
 	if request.user.is_authenticated:
-		profile = Profiles.objects.using('bot').get(pk=request.user.uid)
+		profile = Profiles.objects.using('bot').get(pk=request.user.id)
 	else:
 		profile = None
 	context = {
