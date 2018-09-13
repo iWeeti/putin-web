2567fac73e237828561cf70900ff4b32d60ff2a7
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-13 18:14:23 +0300

    .

--- blog/models.py
+++ blog/models.py
@@ -42,5 +42,5 @@ class Announcement(models.Model):
 	def db_for_write(self, model, **hints):
 		return 'default'
 
-    def db_for_read(self, model, **hints):
+	def db_for_read(self, model, **hints):
 		return 'default'
