0fcabbd6ccead6f33b6afcd9e27eff633028dafd
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-13 09:18:58 +0300

    .

--- putin/models.py
+++ putin/models.py
@@ -8,133 +8,6 @@
 from django.db import models
 
 
-<<<<<<< HEAD
-=======
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=80)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class CommandConfig(models.Model):
-    guild_id = models.BigIntegerField(blank=True, null=True)
-    channel_id = models.BigIntegerField(blank=True, null=True)
-    name = models.TextField(blank=True, null=True)
-    whitelist = models.BooleanField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'command_config'
-        unique_together = (('channel_id', 'name', 'whitelist'),)
-
-
-class Commands(models.Model):
-    guild_id = models.BigIntegerField(blank=True, null=True)
-    channel_id = models.BigIntegerField(blank=True, null=True)
-    author_id = models.BigIntegerField(blank=True, null=True)
-    used = models.DateTimeField(blank=True, null=True)
-    prefix = models.TextField(blank=True, null=True)
-    command = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'commands'
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
 class EmojiStats(models.Model):
     guild_id = models.BigIntegerField(blank=True, null=True)
     emoji_id = models.BigIntegerField(blank=True, null=True)
@@ -146,15 +19,6 @@ class EmojiStats(models.Model):
         unique_together = (('guild_id', 'emoji_id'),)
 
 
-class English(models.Model):
-    ident = models.TextField(blank=True, null=True)
-    message = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'english'
-
-
 class Feeds(models.Model):
     id = models.AutoField()
     channel_id = models.BigIntegerField(blank=True, null=True)
@@ -164,16 +28,6 @@ class Feeds(models.Model):
     class Meta:
         managed = False
         db_table = 'feeds'
-
-
-class Finnish(models.Model):
-    id = models.AutoField()
-    ident = models.TextField(blank=True, null=True)
-    message = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'finnish'
 
 
 class GuildModConfig(models.Model):
@@ -358,14 +212,3 @@ class Tags(models.Model):
     class Meta:
         managed = False
         db_table = 'tags'
-
-
-class Turkish(models.Model):
-    id = models.AutoField()
-    ident = models.TextField(blank=True, null=True)
-    message = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'turkish'
->>>>>>> 353343a5d554b5ae7f99e694d85a616ab876fcf1
