353343a5d554b5ae7f99e694d85a616ab876fcf1
veeti <maaninkaveeti@gmail.com>
2018-09-13 06:16:20 +0000

    ,

--- putin/models.py
+++ putin/models.py
@@ -16,16 +16,6 @@ class AuthGroup(models.Model):
         db_table = 'auth_group'
 
 
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
 class AuthPermission(models.Model):
     name = models.CharField(max_length=255)
     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
@@ -74,68 +64,29 @@ class AuthUserUserPermissions(models.Model):
         unique_together = (('user', 'permission'),)
 
 
-class BlogAnnouncement(models.Model):
-    announcement = models.TextField()
-    date = models.DateTimeField()
-    author = models.ForeignKey(AuthUser, models.DO_NOTHING)
+class CommandConfig(models.Model):
+    guild_id = models.BigIntegerField(blank=True, null=True)
+    channel_id = models.BigIntegerField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    whitelist = models.BooleanField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'blog_announcement'
+        db_table = 'command_config'
+        unique_together = (('channel_id', 'name', 'whitelist'),)
 
 
-class BlogPost(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField()
-    author = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'blog_post'
-
-
-class DiscordBindDiscordinvite(models.Model):
-    code = models.CharField(unique=True, max_length=32)
-    active = models.BooleanField()
-    description = models.CharField(max_length=256)
-    guild_name = models.CharField(max_length=254)
-    guild_id = models.CharField(max_length=20)
-    guild_icon = models.CharField(max_length=254)
-    channel_name = models.CharField(max_length=254)
-    channel_id = models.CharField(max_length=20)
-    channel_type = models.CharField(max_length=5)
+class Commands(models.Model):
+    guild_id = models.BigIntegerField(blank=True, null=True)
+    channel_id = models.BigIntegerField(blank=True, null=True)
+    author_id = models.BigIntegerField(blank=True, null=True)
+    used = models.DateTimeField(blank=True, null=True)
+    prefix = models.TextField(blank=True, null=True)
+    command = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'discord_bind_discordinvite'
-
-
-class DiscordBindDiscordinviteGroups(models.Model):
-    discordinvite = models.ForeignKey(DiscordBindDiscordinvite, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'discord_bind_discordinvite_groups'
-        unique_together = (('discordinvite', 'group'),)
-
-
-class DiscordBindDiscorduser(models.Model):
-    uid = models.CharField(unique=True, max_length=20)
-    username = models.CharField(max_length=254)
-    discriminator = models.CharField(max_length=4)
-    avatar = models.CharField(max_length=254)
-    email = models.CharField(max_length=254)
-    access_token = models.CharField(max_length=254)
-    refresh_token = models.CharField(max_length=254)
-    scope = models.CharField(max_length=256)
-    expiry = models.DateTimeField(blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING, unique=True)
-
-    class Meta:
-        managed = False
-        db_table = 'discord_bind_discorduser'
+        db_table = 'commands'
 
 
 class DjangoAdminLog(models.Model):
@@ -182,10 +133,236 @@ class DjangoSession(models.Model):
         db_table = 'django_session'
 
 
-class UsersProfile(models.Model):
-    image = models.CharField(max_length=100)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING, unique=True)
+class EmojiStats(models.Model):
+    guild_id = models.BigIntegerField(blank=True, null=True)
+    emoji_id = models.BigIntegerField(blank=True, null=True)
+    total = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'users_profile'
+        db_table = 'emoji_stats'
+        unique_together = (('guild_id', 'emoji_id'),)
+
+
+class English(models.Model):
+    ident = models.TextField(blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'english'
+
+
+class Feeds(models.Model):
+    id = models.AutoField()
+    channel_id = models.BigIntegerField(blank=True, null=True)
+    role_id = models.BigIntegerField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'feeds'
+
+
+class Finnish(models.Model):
+    id = models.AutoField()
+    ident = models.TextField(blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'finnish'
+
+
+class GuildModConfig(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    raid_mode = models.SmallIntegerField(blank=True, null=True)
+    broadcast_channel = models.BigIntegerField(blank=True, null=True)
+    mention_count = models.SmallIntegerField(blank=True, null=True)
+    safe_mention_channel_ids = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'guild_mod_config'
+
+
+class IndexAnnouncement(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=500)
+    pub_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'index_announcement'
+
+
+class IndexChoice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField()
+    question = models.ForeignKey('IndexQuestion', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'index_choice'
+
+
+class IndexQuestion(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'index_question'
+
+
+class Plonks(models.Model):
+    guild_id = models.BigIntegerField(blank=True, null=True)
+    entity_id = models.BigIntegerField(unique=True, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'plonks'
+
+
+class Profiles(models.Model):
+    id = models.BigIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    bday = models.TextField(blank=True, null=True)
+    picks = models.DecimalField(max_digits=1000, decimal_places=0, blank=True, null=True)
+    rings = models.DecimalField(max_digits=1000, decimal_places=0, blank=True, null=True)
+    diamonds = models.DecimalField(max_digits=1000, decimal_places=0, blank=True, null=True)
+    roses = models.DecimalField(max_digits=1000, decimal_places=0, blank=True, null=True)
+    alcohol = models.DecimalField(max_digits=1000, decimal_places=0, blank=True, null=True)
+    experience = models.DecimalField(max_digits=1000, decimal_places=0, blank=True, null=True)
+    cash = models.DecimalField(max_digits=1000, decimal_places=0, blank=True, null=True)
+    married = models.BigIntegerField(blank=True, null=True)
+    pet = models.TextField(blank=True, null=True)
+    banner = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'profiles'
+
+
+class Reminders(models.Model):
+    expires = models.DateTimeField(unique=True, blank=True, null=True)
+    created = models.DateTimeField(blank=True, null=True)
+    event = models.TextField(blank=True, null=True)
+    extra = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'reminders'
+
+
+class Rtfm(models.Model):
+    user_id = models.BigIntegerField(unique=True, blank=True, null=True)
+    count = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'rtfm'
+
+
+class Settings(models.Model):
+    id = models.BigIntegerField(blank=True, null=True)
+    message_delete = models.BooleanField(blank=True, null=True)
+    message_edit = models.BooleanField(blank=True, null=True)
+    kick = models.BooleanField(blank=True, null=True)
+    ban = models.BooleanField(blank=True, null=True)
+    log_join = models.BooleanField(blank=True, null=True)
+    leave = models.BooleanField(blank=True, null=True)
+    welcome_enabled = models.BooleanField(blank=True, null=True)
+    welcome_channel = models.BigIntegerField(blank=True, null=True)
+    log_commands = models.BooleanField(blank=True, null=True)
+    unban = models.BooleanField(blank=True, null=True)
+    buy_roles = models.BooleanField(blank=True, null=True)
+    advert = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'settings'
+
+
+class Starboard(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    channel_id = models.BigIntegerField(blank=True, null=True)
+    threshold = models.IntegerField()
+    locked = models.BooleanField(blank=True, null=True)
+    max_age = models.TextField()  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'starboard'
+
+
+class StarboardEntries(models.Model):
+    bot_message_id = models.BigIntegerField(blank=True, null=True)
+    message_id = models.BigIntegerField(unique=True)
+    channel_id = models.BigIntegerField(blank=True, null=True)
+    author_id = models.BigIntegerField(blank=True, null=True)
+    guild = models.ForeignKey(Starboard, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'starboard_entries'
+
+
+class Starrers(models.Model):
+    author_id = models.BigIntegerField()
+    entry = models.ForeignKey(StarboardEntries, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'starrers'
+        unique_together = (('author_id', 'entry'),)
+
+
+class Store(models.Model):
+    id = models.BigIntegerField(blank=True, null=True)
+    price = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    seller_id = models.BigIntegerField(blank=True, null=True)
+    selling_id = models.AutoField()
+    quantity = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    item_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'store'
+
+
+class TagLookup(models.Model):
+    name = models.TextField(blank=True, null=True)
+    location_id = models.BigIntegerField(blank=True, null=True)
+    owner_id = models.BigIntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    tag = models.ForeignKey('Tags', models.DO_NOTHING, blank=True, null=True)
+
+    # A unique constraint could not be introspected.
+    class Meta:
+        managed = False
+        db_table = 'tag_lookup'
+
+
+class Tags(models.Model):
+    name = models.TextField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    owner_id = models.BigIntegerField(blank=True, null=True)
+    uses = models.IntegerField(blank=True, null=True)
+    location_id = models.BigIntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+
+    # A unique constraint could not be introspected.
+    class Meta:
+        managed = False
+        db_table = 'tags'
+
+
+class Turkish(models.Model):
+    id = models.AutoField()
+    ident = models.TextField(blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'turkish'
