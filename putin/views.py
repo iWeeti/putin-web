1601767cab90ad7da08b8e64940649a37b229ee3
Veeti Maaninka <maaninkaveeti@gmail.com>
2018-09-15 09:55:09 +0300

    .

--- putin/views.py
+++ putin/views.py
@@ -56,7 +56,7 @@ def guilds(request):
 	for guild in guilds:
 		guild_perms = discord.Permissions(int(guild['permissions']))
 		print(guild_perms.manage_guild)
-		if guild['id'] in str(bot_guilds_ids) and guild_perms.manage_guild:
+		if guild['id'] in str(bot_guilds_ids) and guild_perms.manage_guild or guild_perms.administrator or guild_perms.manage_channels:
 			_guilds.append(guild)
 	context = {
 		'ann': ann,
