5b4c9d5f72972fb5b01f15b2316ffde8ba489378
iWeeti <maaninkaveeti@gmail.com>
2018-09-30 00:32:26 +0300

    .

--- putin/views.py
+++ putin/views.py
@@ -66,16 +66,11 @@ def guilds(request):
 
 def dashboard(request):
 	if request.method == 'POST':
-		# form = SettingsForm(request.POST)
 		ann = Announcement.objects.all()[::-1]
 		_settings = Settings.objects.using('bot').get(pk=request.GET['id'])
-		if request.POST['kick'] == 1:
-			_settings.kick = True
-		elif request.POST['kick'] == 2:
-			_settings.kick = False
-		_settings.save()
+		form = SettingsForm(request.POST, instance=_settings)
 		# .get(id=int(request.GET['id']))
-		form = SettingsForm(instance=_settings)
+		# form = SettingsForm(instance=_settings)
 		context = {
 			'ann': ann,
 			'guild_id': request.GET['id'],
