a304f304221f9d914520ad4877cd7b096091b288
iWeeti <maaninkaveeti@gmail.com>
2018-09-30 01:12:03 +0300

    .

--- putin/forms.py
+++ putin/forms.py
@@ -4,4 +4,4 @@ from .models import Settings
 class SettingsForm(forms.ModelForm):
 	class Meta:
 		model = Settings
-		fields = ['leave', 'kick', 'ban', 'welcome_enabled', 'advert', 'message_delete', 'message_edit', 'buy_roles', 'log_commands']
+		fields = ['leave', 'kick', 'ban', 'welcome_enabled', 'advert', 'message_delete', 'message_edit', 'buy_roles', 'log_commands', 'logging_channel']
