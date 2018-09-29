63be93f39cb5a68e05c00674fda60e1455d00496
iWeeti <maaninkaveeti@gmail.com>
2018-09-29 16:01:57 +0300

    trying to make image serving app

--- putin_web/urls.py
+++ putin_web/urls.py
@@ -14,6 +14,4 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
 	path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
-
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
