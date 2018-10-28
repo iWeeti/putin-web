b410c9b1bd5ac91f8fb08af05460e912dfe553a9
iWeeti <maaninkaveeti@gmail.com>
2018-10-28 15:08:39 +0000

    asdf

--- putin/views.py
+++ putin/views.py
@@ -123,7 +123,8 @@ def dashboard(request):
 		# .get(id=int(request.GET['id']))
 		form = SettingsForm(instance=_settings)
 		guild = requests.get(f'https://discordapp.com/api/guilds/{request.GET.get("id")}', headers={'Authorization': 'Bot ' + config.token}).json()
-		print(guild)
+		for obj in guild:
+			print(obj)
 		context = {
 			'ann': ann,
 			'guild_id': request.GET['id'],
