a65c97ff7ea4d681202c0624d4d6e14ef33c086f
iWeeti <maaninkaveeti@gmail.com>
2018-10-28 13:58:55 +0000

    hmm

--- putin/forms.py
+++ putin/forms.py
@@ -4,4 +4,26 @@ from .models import Settings
 class SettingsForm(forms.ModelForm):
 	class Meta:
 		model = Settings
-		fields = ['leave', 'kick', 'ban', 'unban', 'welcome_enabled', 'advert', 'message_delete', 'message_edit', 'buy_roles', 'log_commands', 'logging_channel']
+		fields = [
+			'leave',
+			'kick',
+			'ban',
+			'unban',
+			'advert',
+			'message_delete',
+			'message_edit',
+			'buy_roles',
+			'log_commands',
+			'logging_channel'
+			]
+		labels = {
+			'leave': 'Leave Logging',
+			'kick': 'Kick Logging',
+			'ban': 'Ban Logging',
+			'unban': 'UnBan Logging',
+			'message_delete': 'Message Delete Logging',
+			'message_edit': 'Messsage Edit Logging',
+			'buy_roles': 'Buyable roles from shop.',
+			'log_commands': 'Log Command Uses',
+			'logging_channel': 'Logging Channel'
+		}
